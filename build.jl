638d2bd5ae57075aef8cfc4e5ba4bfa12381986d
Martin Morin <martin.morin@control.lth.se>
2022-01-10 23:22:22 +0100

    Make comilation pure julia script

--- build.jl
+++ build.jl
@@ -12,3 +12,8 @@ create_app(src_dir, output_dir, force=true)
 # name = "Queller"*(Sys.iswindows() ? ".exe" : "")
 # path = joinpath("bin", name)
 # link = joinpath(output_dir, name)
+
+# tar = `tar -czvf $(output_dir).tar.gz $(output_dir)`
+# zip = `zip -r $(output_dir).zip $(output_dir)`
+# run(tar)
+# run(zip)
