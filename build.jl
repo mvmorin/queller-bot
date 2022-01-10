d03d7986e26f9b39c3c9984670fe38018fb85342
Martin Morin <martin.morin@control.lth.se>
2022-01-10 22:29:36 +0100

    Try to build for real

--- build.jl
+++ build.jl
@@ -1,28 +1,17 @@
-# src_dir="Queller"
-# output_dir="QuellerCLI"
+src_dir="Queller"
+output_dir="QuellerCLI"
 
-# using Pkg
-# Pkg.activate(src_dir)
-# Pkg.instantiate()
+using Pkg
+Pkg.activate(src_dir)
+Pkg.instantiate()
 
-# using PackageCompiler
+using PackageCompiler
+
+create_app(src_dir, output_dir, force=true)
 
 # name = "Queller"*(Sys.iswindows() ? ".exe" : "")
 # path = joinpath("bin", name)
 # link = joinpath(output_dir, name)
 
-# create_app(src_dir, output_dir, force=true)
-
 # rm(link)
 # symlink(path, link, dir_target=false)
-
-# tar = `tar -czvf $(output_dir).tar.gz $(output_dir)`
-# zip = `zip -r $(output_dir).zip $(output_dir)`
-# run(tar)
-# run(zip)
-
-println("Hello, World")
-mkpath("build-output/tjo")
-write("build-output/test.txt", "hej\n")
-write("build-output/test2.txt", "hallå\n")
-write("build-output/tjo/test.txt", "tjena\n")
