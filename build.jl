0726882712466efa87f1f5948af60fd2b0bc3051
Martin Morin <martin.morin@control.lth.se>
2022-01-10 21:33:17 +0100

    run julia script is action

--- build.jl
+++ build.jl
@@ -1,22 +1,24 @@
-src_dir="Queller"
-output_dir="QuellerCLI"
+# src_dir="Queller"
+# output_dir="QuellerCLI"
 
-using Pkg
-Pkg.activate(src_dir)
-Pkg.instantiate()
+# using Pkg
+# Pkg.activate(src_dir)
+# Pkg.instantiate()
 
-using PackageCompiler
+# using PackageCompiler
 
-name = "Queller"*(Sys.iswindows() ? ".exe" : "")
-path = joinpath("bin", name)
-link = joinpath(output_dir, name)
+# name = "Queller"*(Sys.iswindows() ? ".exe" : "")
+# path = joinpath("bin", name)
+# link = joinpath(output_dir, name)
 
-create_app(src_dir, output_dir, force=true)
+# create_app(src_dir, output_dir, force=true)
 
-rm(link)
-symlink(path, link, dir_target=false)
+# rm(link)
+# symlink(path, link, dir_target=false)
 
-tar = `tar -czvf $(output_dir).tar.gz $(output_dir)`
-zip = `zip -r $(output_dir).zip $(output_dir)`
-run(tar)
-run(zip)
+# tar = `tar -czvf $(output_dir).tar.gz $(output_dir)`
+# zip = `zip -r $(output_dir).zip $(output_dir)`
+# run(tar)
+# run(zip)
+
+println("Hello, World")
