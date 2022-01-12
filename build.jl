120be1970b08e222bab661e9917fe1877d2d73b5
Martin Morin <martin.morin@control.lth.se>
2022-01-12 23:09:00 +0100

    Add startup script@ to compiled programs

--- build.jl
+++ build.jl
@@ -9,3 +9,18 @@ Pkg.instantiate()
 using PackageCompiler
 
 create_app(src_dir, output_dir, executables=["Queller"=>"main"], force=true)
+
+if Sys.iswindows()
+	startup_body = """
+	@echo off
+	start ./bin/Queller.exe
+	"""
+	startup_file_name = "QuellerCLI.bat"
+else
+	startup_body = """
+	#!/bin/sh
+	./bin/Queller
+	"""
+	startup_file_name = "QuellerCLI"
+end
+write(output_dir*startup_file_name, startup_body)
